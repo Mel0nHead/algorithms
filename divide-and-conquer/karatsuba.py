7ecef6778c6eddd001edc433afb2978d0760942e
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-12-05 18:10:57 +0000

    get karatsuba working

---
+++ divide-and-conquer/karatsuba.py
@@ -0,0 +1,23 @@
+num1 = 3141592653589793238462643383279502884197169399375105820974944592
+num2 = 2718281828459045235360287471352662497757247093699959574966967627
+
+def karatsuba(x,y):
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        return x*y
+    else:
+        m = max(len(str(x)),len(str(y)))
+        m2 = m // 2
+
+        a = x // 10**(m2)
+        b = x % 10**(m2)
+        c = y // 10**(m2)
+        d = y % 10**(m2)
+
+        z0 = karatsuba(b,d)
+        z1 = karatsuba((a+b),(c+d))
+        z2 = karatsuba(a,c)
+
+        return (z2 * 10**(2*m2)) + ((z1 - z2 - z0) * 10**(m2)) + (z0)
+
+print(karatsuba(num1,num2))
+print(num1 * num2)
