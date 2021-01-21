6d6e0d83dc4d0f78d6c5f96a30b8145f99f68d55
Alex King <alex.king@deepcrawl.com>
2021-01-21 12:02:38 +0000

    works, but is way too slow

--- graph-search/two_sum_problem.py
+++ graph-search/two_sum_problem.py
@@ -2,12 +2,13 @@
 # Given an array of n integers, and a value t, does there exist a distinct pair of integers x and y in the array such that x + y = t?
 # In our specific implementation, we need to solve for each t in [-10000, 10000], and count for how many values of t an answer exists
 
-array = [4,6,7,8,5,3,2,111,2,3,2,10000,2,444,547]
 H = {}
+file = open("./two_sum_data.txt","r")
 
 # Insert all elements of the array into a hash table
-for num in array:
-    H[num] = True
+for line in file:
+    print('processing line ' + str(line) + ' of file')
+    H[int(line)] = 0
 
 def two_sum(hashTable, targetValue):
     doesSolutionExist = False
@@ -27,9 +28,10 @@ def iterate_two_sum(hashTable, lowerBound, upperBound):
     totalCount = 0
 
     for t in range(lowerBound, upperBound + 1):
+        print('target value: ' + str(t))
         if two_sum(hashTable, t) == True:
             totalCount += 1
     
     return totalCount
-
-print(iterate_two_sum(H, -10, 10))
+# Currently very slow: takes ~ 2 mins 4 seconds to iterate through 500 values of t
+print('FINAL ANSWER', iterate_two_sum(H, -10000, 10000))
