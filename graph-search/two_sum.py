a38dfa332518d181625e09e339fde47e93ac5052
Alex King <alex.king@deepcrawl.com>
2021-01-22 10:14:33 +0000

    add comments

--- graph-search/two_sum.py
+++ graph-search/two_sum.py
@@ -19,11 +19,19 @@ while i < j:
                 j -= 1
                 break
             else:
-                if x != j:
-                    H[x + j] = 1
-        i += 1
+                if array[x] != array[j]:
+                    H[array[x] + array[j]] = 1
+                if x == j - 1:
+                    i += 1 # maybe j -= 1, not sure 
 
+print(array)
+# FIND t in [3,10]
+# should get 3, 4, 5, 6, 7, 8, 9, 10
+# [-3, -1, 1, 2, 2, 6, 7, 9, 11]
 
-
-
-
+# i = 0, j = 8: sum = 8; iterate through range(i,j) finding 8, 10, 12 (then break and j--)
+# i = 0, j = 7: sum = 6; iterate through range(i,j) finding 6, 8, 10, 11 (then break and j--)
+# i = 0, j = 6: sum = 4; iterate through range(i,j) finding 4, 6, 8, 9, 9, 13 (then break and j--)
+# i = 0, j = 5: sum = 3; iterate through range(i,j) finding 3, 5, 7, 8, 8 (come to end of loop, so i++)
+# i = 1, j = 5: sum = 5; iterate through range(i,j) finding 5, 7, 8, 8 (come to end of loop, so i++)
+# i = 2, j = 
--- graph-search/two_sum_problem.py
+++ graph-search/two_sum_problem.py
@@ -1,7 +1,6 @@
 # This algorithm uses a hash table to solve the 2 sum problem, and therefore has O(n)
 # Given an array of n integers, and a value t, does there exist a distinct pair of integers x and y in the array such that x + y = t?
 # In our specific implementation, we need to solve for each t in [-10000, 10000], and count for how many values of t an answer exists
-
 H = {}
 file = open("./two_sum_data.txt","r")
 
@@ -10,29 +9,21 @@ for line in file:
     print('processing line ' + str(line) + ' of file')
     H[int(line)] = 0
 
-def two_sum(hashTable, targetValue):
-    doesSolutionExist = False
+totalCount = 0
+hashKeys = H.keys()
 
-    # For each value x in the hash table, try and lookup targetValue - x
-    for x in hashTable.keys():
-        y = targetValue - x
-        
-        # If x and targetValue - x exist in the hash table, and x != targetValue - x, then it is a valid solution
-        if x != y and y in hashTable:
-            doesSolutionExist = True
+for t in range(-10000, 10001):
+    print('target value: ' + str(t))
+    
+    for x in hashKeys:
+        y = t - x
+
+        if x != y and y in H:
+            totalCount += 1
             break
     
-    return doesSolutionExist
 
-def iterate_two_sum(hashTable, lowerBound, upperBound):
-    totalCount = 0
-
-    for t in range(lowerBound, upperBound + 1):
-        print('target value: ' + str(t))
-        if two_sum(hashTable, t) == True:
-            totalCount += 1
     
-    return totalCount
-# Currently very slow: takes ~ 2 mins 4 seconds to iterate through 500 values of t
+# Currently very slow: takes ~ 1 min 50 secs to iterate through 500 values of t
 # It will take over 80 mins to complete the whole thing
-print('FINAL ANSWER', iterate_two_sum(H, -10000, 10000))
+print('***** FINAL ANSWER *****', totalCount)
