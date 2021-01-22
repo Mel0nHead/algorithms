85806ab5dd7d715440436f73e953ebb4dc3227d4
Alex King <alex.king@deepcrawl.com>
2021-01-22 11:01:41 +0000

    more stuff

--- graph-search/two_sum.py
+++ graph-search/two_sum.py
@@ -6,7 +6,7 @@ upperBound = 10
 
 i = 0
 j = len(array) - 1
-H = {}
+H = {} # this could be a set
 
 while i < j:
     if array[i] + array[j] < lowerBound:
@@ -15,23 +15,25 @@ while i < j:
         j -= 1
     else:
         for x in range(i,j):
-            if array[x] + array[j] > upperBound:
-                j -= 1
+            numX = array[x]
+            numJ = array[j]
+
+            if numX + numJ > upperBound:
                 break
             else:
-                if array[x] != array[j]:
-                    H[array[x] + array[j]] = 1
-                if x == j - 1:
-                    i += 1 # maybe j -= 1, not sure 
+                if numX != numJ:
+                    H[numX + numJ] = 1
+              
+        j -= 1 # might be dodgy, not sure
 
-print(array)
+print(len(H.keys()))
 # FIND t in [3,10]
 # should get 3, 4, 5, 6, 7, 8, 9, 10
-# [-3, -1, 1, 2, 2, 6, 7, 9, 11]
+# [-2, 0, 0, 4]
 
 # i = 0, j = 8: sum = 8; iterate through range(i,j) finding 8, 10, 12 (then break and j--)
 # i = 0, j = 7: sum = 6; iterate through range(i,j) finding 6, 8, 10, 11 (then break and j--)
 # i = 0, j = 6: sum = 4; iterate through range(i,j) finding 4, 6, 8, 9, 9, 13 (then break and j--)
 # i = 0, j = 5: sum = 3; iterate through range(i,j) finding 3, 5, 7, 8, 8 (come to end of loop, so i++)
 # i = 1, j = 5: sum = 5; iterate through range(i,j) finding 5, 7, 8, 8 (come to end of loop, so i++)
-# i = 2, j = 
+# i = 2, j = 5: sum = 7; iterate through range(i,j) finding 7, 8, 8 (come to end of loop), so i ++
