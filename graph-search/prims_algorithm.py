dabe8b84a4bba2de080f4a0f7d7452787805f3ea
Alex King <alex.king@deepcrawl.com>
2021-01-29 10:34:05 +0000

    start on prims

---
+++ graph-search/prims_algorithm.py
@@ -0,0 +1,7 @@
+# Prim's algorithm
+# Used to find the minimum spanning tree of a connected, undirected graph with weighted edges
+# O(nm) for naive implementation; O(mlog(n)) for heap implementation
+
+# Each edge will be in the format [head_node, tail_node, edge_weight]
+
+# Pick an arbitary vertex to start
