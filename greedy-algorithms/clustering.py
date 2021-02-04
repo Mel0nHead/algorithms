01f71020a6075913430a93bb08f1fd9a0a67caac
Alex King <alex.king@deepcrawl.com>
2021-02-04 15:40:24 +0000

    finish clustering algo

--- greedy-algorithms/clustering.py
+++ greedy-algorithms/clustering.py
@@ -1,6 +1,7 @@
 # Clustering algorithm
 # If we have a set of points, group the points into k clusters such that we maximise the spacing
-# The `spacing` of a clustering is the distance between the closest two seperated (in different clusters) points 
+# The `spacing` of a clustering is the distance between the closest two seperated (in different clusters) points
+import dset
 
 file = open('./clustering.txt', 'r')
 k = 4 # target number of clusters
@@ -8,7 +9,7 @@ numberOfClusters = 0
 edges = []
 
 for line in file:
-    numberArray = [int(i) for i in line]
+    numberArray = [int(i) for i in line.split()]
 
     if len(numberArray) == 1:
         numberOfClusters = numberArray[0] # Initially, each point will be in its own cluster
@@ -16,11 +17,31 @@ for line in file:
         # Each edge is in the format [node_a, node_b, distance]
         edges.append(numberArray)
 
-# Sort the `edges` in decreasing order of weight - this has O(mlog(n))
+def getWeight(edge):
+    return edge[2]
 
+# Sort the `edges` in increasing order of weight - this operation has O(mlog(n))
+edges.sort(key=getWeight, reverse=False)
+nodeClusters = [dset.Set() for i in range(0,numberOfClusters)] # node 1 at index 0, node 2 at index 1 etc...
 
-# Use a Union-Find data structure to track which node is in which cluster
+while numberOfClusters > k:
+    for e in edges:
+        [nodeA, nodeB, weight] = e
+        nodeAGroup, nodeBGroup = nodeClusters[nodeA - 1], nodeClusters[nodeB - 1]
 
-# while the number of clusters > k
-# Find the closest pair of seperated points
-# Merge the two clusters containing these two points
+        # Find the closest pair of seperated points
+        if dset.find(nodeAGroup) != dset.find(nodeBGroup):
+            # Merge the two clusters containing these two points
+            dset.union(nodeAGroup, nodeBGroup)
+            numberOfClusters -= 1
+            break
+
+for e in edges:
+    [nodeA, nodeB, weight] = e
+    nodeAGroup, nodeBGroup = nodeClusters[nodeA - 1], nodeClusters[nodeB - 1]
+
+    # Find the closest pair of seperated points
+    if dset.find(nodeAGroup) != dset.find(nodeBGroup):
+        # Merge the two clusters containing these two points
+        print(weight)
+        break
